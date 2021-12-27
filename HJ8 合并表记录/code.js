6e99fbd0d7b7abdc2f59aef093408fe5fa80e9db
dontworrMac <562930531@qq.com>
2021-12-28 01:01:37 +0800

    HJ8 合并表记录

--- HJ8 合并表记录/code.js
+++ HJ8 合并表记录/code.js
@@ -25,29 +25,25 @@ function deal(inputs) {
     var kvStr = inputs[i].split(" ");
     var kv = [parseInt(kvStr[0]), parseInt(kvStr[1])];
     var existB = false;
+    var index = 0;
     for (let j = 0; j < keyVs.length; j++) {
       var existInt = keyVs[j][0];
       if (kv[0] === existInt) {
         keyVs[j][1] += kv[1];
         existB = true;
-      } else {
+      } else if (kv[0] > existInt) {
+        index = j + 1;
       }
     }
-    if (!existB){
-      var endKey = keyVs[keyVs.length - 1][0];
-      var addKey = kv[0];
-      for (let i = 0; i < keyVs.length; i++) {
-        const element = array[i];
-        
-      }
-      keyVs.push(kv);
-    } 
+    if (!existB) {
+      keyVs.splice(index,0,kv);
+    }
   }
   for (let i = 0; i < keyVs.length; i++) {
     console.log(keyVs[i][0] + " " + keyVs[i][1]);
   }
 }
 
-var ss = ["0 1", "0 2", "1 2", "3 4"];
+var ss = ["0 1", "1 2", "0 2", "3 4"];
 
 deal(ss);
