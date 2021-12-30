d9c177ee53345c36ce16bfdbfb081f683fdd8afe
Lee <mi57404037zha@163.com>
2021-12-30 20:19:41 +0800

    unfinished 学英语

--- HJ42 学英语/code.js
+++ HJ42 学英语/code.js
@@ -52,9 +52,48 @@ var twieties = [
 
 var ranges = ["hundred", "thousand", "million", "billion"];
 function algorithmExe(inputArr) {
+  var numStr = inputArr[0].split("");
   var num = Number(inputArr[0]);
+  console.log(hundredNum(num));
 }
 
+function hundredNum(num) {
+  var useTens = false;
+  var outStr = [];
+  for (let i = 1; i <= 3; i++) {
+    var res1 = num % Math.pow(10, i);
+    var res = res1 / Math.pow(10, i - 1);
+    num = num - res * Math.pow(10, i - 1);
+    if (i === 1) {
+      if (res !== 0) {
+        outStr.push(ones[res]);
+      } else {
+        useTens = true;
+      }
+    }
+    if (i === 2) {
+      if (res !== 0) {
+        if (useTens) {
+          outStr.push(tens[res]);
+        } else {
+          outStr.push(twieties[res]);
+        }
+      } else {
+      }
+    }
+    if (i === 3) {
+      if (res !== 0) {
+        outStr.length > 0 && outStr.push("and");
+        outStr.push("hundred");
+        outStr.push(ones[res]);
+      }
+    }
+  }
+  return outStr.reverse().join(" ");
+}
+
+algorithmExe(["123"]);
 algorithmExe(["100"]);
+algorithmExe(["101"]);
 
 process.exit();
