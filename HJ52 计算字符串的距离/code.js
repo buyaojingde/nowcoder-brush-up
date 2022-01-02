e9b87622389e1a8878a2f01c4bb4e27ae07be058
dontworrMac <562930531@qq.com>
2022-01-02 19:24:57 +0800

    HJ52 计算字符串的距离

--- HJ52 计算字符串的距离/code.js
+++ HJ52 计算字符串的距离/code.js
@@ -14,9 +14,34 @@ rl.on("line", function (input) {
 });
 
 function algorithmExe(inputArr) {
-  const inStr1 = inputArr[0];
-  const inStr2 = inputArr[1];
-  
+  const inStr1 = inputArr[0].split("");
+  const inStr2 = inputArr[1].split("");
+  //子串相同的个数
+  var mat = [];
+  for (let j = 0; j <= inStr1.length; j++) {
+    var tmp = [];
+    mat.push(tmp);
+  }
+  for (let i = 0; i <= inStr1.length; i++) {
+    mat[i][0] = i;
+  }
+  for (let j = 0; j <= inStr2.length; j++) {
+    mat[0][j] = j;
+  }
+
+  for (let i = 1; i <= inStr1.length; i++) {
+    for (let j = 1; j <= inStr2.length; j++) {
+      if (inStr1[i - 1] === inStr2[j - 1]) {
+        mat[i][j] = mat[i - 1][j - 1];
+      } else {
+        mat[i][j] =
+          Math.min(mat[i - 1][j], mat[i][j - 1], mat[i - 1][j - 1]) + 1;
+      }
+    }
+  }
+  var res = mat[inStr1.length][inStr2.length];
+  console.log(res);
 }
 
+algorithmExe(["abcdefg", "abcdef"]);
 process.exit();
