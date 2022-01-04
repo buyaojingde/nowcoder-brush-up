f1e1db012313a32666d7dac4e7aa2a9dc0bb5f0b
Lee <mi57404037zha@163.com>
2022-01-04 10:27:42 +0800

    finished: HJ65 查找两个字符串a,b中的最长公共子串

--- HJ65 查找两个字符串a,b中的最长公共子串/code.js
+++ HJ65 查找两个字符串a,b中的最长公共子串/code.js
@@ -17,5 +17,25 @@ rl.on("line", function (input) {
 function algorithmExe(inputArr) {
   var subStr = inputArr[0];
   var subStr1 = inputArr[1];
+  var short = subStr.length < subStr1.length ? subStr : subStr1;
+  var longStr = subStr.length < subStr1.length ? subStr1 : subStr;
+  var max = 0;
+  var maxSubStr = null;
+  for (let index = 0; index < short.length; index++) {
+    for (let j = index; j <= short.length; j++) {
+      var subStrTmp = short.substring(index, j);
+      if (longStr.includes(subStrTmp)) {
+        if (subStrTmp.length > max) {
+          max = subStrTmp.length;
+          maxSubStr = subStrTmp;
+        }
+      }
+    }
+  }
+  if (maxSubStr) {
+    console.log(maxSubStr);
+  }
 }
+
+algorithmExe(["abcdefghijklmnop", "abcsafjklmnopqrstuvw"]);
 process.exit();
