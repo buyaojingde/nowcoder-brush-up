d17cb2919d57bd03bee61988a5251283062378fa
dontworrMac <562930531@qq.com>
2022-01-05 23:16:03 +0800

    finished: HJ74 参数解析

--- HJ74 参数解析/code.js
+++ HJ74 参数解析/code.js
@@ -16,32 +16,37 @@ rl.on("line", function (input) {
 
 function algorithmExe(inputArr) {
   var inputStr = inputArr[0];
-  var start = -1;
-  var startBlank = 0;
   var arr = [];
   var tmp = [];
   for (let i = 0; i < inputStr.length; i++) {
-    if (inputArr[i] === " ") {
-      if (tmp.length > 0 && tmp[0] !== '"') {
-        arr.push(tmp.join(""));
-        tmp = [];
-      }
-    } else if (inputArr[i] === '"') {
+    if (tmp.length > 0) {
       if (tmp[0] === '"') {
-        arr.push(tmp.slice(1).join(""));
-        tmp = [];
+        if (inputStr[i] !== '"') {
+          tmp.push(inputStr[i]);
+        } else {
+          arr.push(tmp.slice(1).join(""));
+          tmp = [];
+        }
       } else {
-        tmp.push(inputArr[i]);
+        if (inputStr[i] === " ") {
+          arr.push(tmp.join(""));
+          tmp = [];
+        } else {
+          tmp.push(inputStr[i]);
+        }
       }
     } else {
-      tmp.push(inputArr[i]);
+      if (inputStr[i] !== " ") {
+        tmp.push(inputStr[i]);
+      }
     }
   }
-  arr.push(tmp.join(""));
+  if (tmp.length > 0) arr.push(tmp.join(""));
   console.log(arr.length);
   for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
   }
 }
 algorithmExe(['xcopy /s "C:\\program files" "d:"']);
+// algorithmExe(['u "a e i o u" r']);
 process.exit();
