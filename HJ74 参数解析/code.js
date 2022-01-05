f92a1806e4a0857e7e3056eec275a69773c4c18b
Lee <mi57404037zha@163.com>
2022-01-05 19:41:59 +0800

    unfinished: HJ74 参数解析

--- HJ74 参数解析/code.js
+++ HJ74 参数解析/code.js
@@ -14,6 +14,34 @@ rl.on("line", function (input) {
   }
 });
 
-function algorithmExe(inputArr) {}
-
+function algorithmExe(inputArr) {
+  var inputStr = inputArr[0];
+  var start = -1;
+  var startBlank = 0;
+  var arr = [];
+  var tmp = [];
+  for (let i = 0; i < inputStr.length; i++) {
+    if (inputArr[i] === " ") {
+      if (tmp.length > 0 && tmp[0] !== '"') {
+        arr.push(tmp.join(""));
+        tmp = [];
+      }
+    } else if (inputArr[i] === '"') {
+      if (tmp[0] === '"') {
+        arr.push(tmp.slice(1).join(""));
+        tmp = [];
+      } else {
+        tmp.push(inputArr[i]);
+      }
+    } else {
+      tmp.push(inputArr[i]);
+    }
+  }
+  arr.push(tmp.join(""));
+  console.log(arr.length);
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+algorithmExe(['xcopy /s "C:\\program files" "d:"']);
 process.exit();
