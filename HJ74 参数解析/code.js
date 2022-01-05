06ff4ef903fb6ad423cfe8b9710c2f65c3798cb5
Lee <mi57404037zha@163.com>
2022-01-05 17:51:48 +0800

    unfinished: HJ74 参数解析

---
+++ HJ74 参数解析/code.js
@@ -0,0 +1,19 @@
+var readLine = require("readline");
+rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+var lineNum = 1;
+var inputs = [];
+rl.on("line", function (input) {
+  inputs.push(input.trim());
+  if (inputs.length === lineNum) {
+    algorithmExe(inputs);
+    inputs = [];
+  }
+});
+
+function algorithmExe(inputArr) {}
+
+process.exit();
