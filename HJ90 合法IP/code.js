42d06c6467d2d1d71571df721149e6ec69ef2d4d
dontworrMac <562930531@qq.com>
2022-01-06 23:56:53 +0800

    finished: HJ90 合法IP

--- HJ90 合法IP/code.js
+++ HJ90 合法IP/code.js
@@ -15,7 +15,46 @@ rl.on("line", function (input) {
 });
 
 function algorithmExe(inputArr) {
-  var ipStr = inputArr[0];
+  var ipStr = inputArr[0].split(".");
+  if (ipStr.length !== 4) {
+    console.log("NO");
+    return;
+  }
+  for (let index = 0; index < ipStr.length; index++) {
+    const element = ipStr[index];
+    if (element.length > 0) {
+      for (let i = 0; i < element.length; i++) {
+        const charItem = element[i];
+        if (charItem.charCodeAt() >= 48 && charItem.charCodeAt() <= 57) {
+        } else {
+          console.log("NO");
+          return;
+        }
+      }
+    }
+    if (element.length > 1) {
+      if (element[0] === "0") {
+        console.log("NO");
+        return;
+      }
+    }
+    if (element === "") {
+      console.log("NO");
+      return;
+    }
+    const ipItem = Number(element);
+    if (isNaN(ipItem)) {
+      console.log("NO");
+      return;
+    }
+    if (ipItem >= 0 && ipItem <= 255) {
+    } else {
+      console.log("NO");
+      return;
+    }
+  }
+  console.log("YES");
 }
-algorithmExe(["10.138.15.1"]);
+// algorithmExe(["10.138.15.1"]);
+algorithmExe(["01.2.3.8"]);
 process.exit();
