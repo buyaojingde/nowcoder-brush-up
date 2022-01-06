7bbd63486b9f7f403b225fda72bd69455873657b
dontworrMac <562930531@qq.com>
2022-01-07 00:18:47 +0800

    finished: HJ91 走方格的方案数

--- HJ91 走方格的方案数/code.js
+++ HJ91 走方格的方案数/code.js
@@ -16,6 +16,30 @@ rl.on("line", function (input) {
 
 function algorithmExe(inputArr) {
   var [m, n] = inputArr[0].split(" ").map((item) => Number(item));
+  var x = 0,
+    y = 0;
+  var total = [];
+  var path = [[0, 0]];
+  go(x, y, m, n, path, total);
+  console.log(total.length);
+}
+
+function go(x, y, endX, endY, path, totalPath) {
+  if (x === endX && y === endY) {
+    totalPath.push(path);
+    return;
+  }
+  if (x < endX) {
+    var newPath = [...path];
+    newPath.push([x + 1, y]);
+    go(x + 1, y, endX, endY, newPath, totalPath);
+  }
+
+  if (y < endY) {
+    var newPath1 = [...path];
+    newPath1.push([x, y + 1]);
+    go(x, y + 1, endX, endY, newPath1, totalPath);
+  }
 }
 algorithmExe(["2 2"]);
 process.exit();
