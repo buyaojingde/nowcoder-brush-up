19a654a79f699918c38fa725c9140b145f2246c0
dontworrMac <562930531@qq.com>
2022-01-06 11:21:26 +0800

    unfinished: HJ88 扑克牌大小

---
+++ HJ88 扑克牌大小/code.js
@@ -0,0 +1,21 @@
+var readLine = require("readline");
+rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+var lineNum = 1;
+var inputs = [];
+rl.on("line", function (input) {
+  inputs.push(input.trim());
+  if (inputs.length === lineNum) {
+    algorithmExe(inputs);
+    inputs = [];
+  }
+});
+
+function algorithmExe(inputArr) {
+  var puke = inputArr[0];
+}
+algorithmExe(["4 4 4 4-joker JOKER"]);
+process.exit();
