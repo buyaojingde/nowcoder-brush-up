040540b461e9148cbd638ce1b5fb61e08c51ba9d
dontworrMac <562930531@qq.com>
2022-01-19 01:29:25 +0800

    调试c++代码

--- HJ6 质数因子/cppCode.cpp
+++ HJ6 质数因子/cppCode.cpp
@@ -1,24 +1,33 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
+void zsyz(int num)
+{
+    int maxi = sqrt(num);
+    for (int i = 2; i <= maxi; i++)
+    {
+        while (!(num % i))
+        {
+            num /= i;
+            printf("%d ", i);
+        }
+    }
+    if (num != 1)
+    {
+        printf("%d ", num);
+    }
+
+    printf("\n");
+}
 int main()
 {
     long num = 0;
     int i;
 
-    while (scanf("%ld", &num) != EOF) {
-        int maxi = sqrt(num);
-        for (i = 2; i <= maxi; i++) {
-            while (!(num % i)) {
-                num /= i;
-                printf("%d ", i);
-            }
-        }
-        if (num != 1) {
-            printf("%d ", num);
-        }
-
-        printf("\n");
+    while (scanf("%ld", &num) != EOF)
+    {
+        zsyz(num);
     }
+    // zsyz(180);
     return 0;
 }
